b95b7e8fa424d8251707211a15fc3ec860f1be57
kiranFullStack <kiran.fullstack@outlook.com>
2023-05-14 22:45:12 +0530

    Components created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Component22 from './Component22'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <h1>App</h1>
+      <Component22 />
+      <Footer />
     </div>
-  );
+  )
 }
-
-export default App;
